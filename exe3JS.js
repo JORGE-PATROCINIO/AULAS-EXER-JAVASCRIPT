266c9a1a15320bbb64e46c6b2bfecf82e0b5a36b
JORGE PATROCÍNIO <103794898+JORGE-PATROCINIO@users.noreply.github.com>
2023-09-18 23:06:44 -0300

    Update exe3JS.js

--- exe3JS.js
+++ exe3JS.js
@@ -1,7 +1,8 @@
-/* Escreva um programa para ler o ano de nascimento de uma pessoa e escrever uma mensagem que diga se ela poderá ou não votar este ano (não é necessário considerar o mês em que ela nasceu). */
+/* Escreva um programa para ler o ano de nascimento de uma pessoa e escrever uma mensagem que diga
+se ela poderá ou não votar este ano (não é necessário considerar o mês em que ela nasceu). */
 
-let x = prompt("Digite o ano de seu nascimento com 4 digítos: ");
-let y = prompt("Digite o ano atual com 4 digítos:") ;
+var x = prompt("Digite o ano de seu nascimento com 4 digítos: ");
+var y = prompt("Digite o ano atual com 4 digítos:") ;
 
 idade = y-x;
 
